08eaab6c6580bf940b72088e58b71259da03e68b
Andreas Meghoo <andyjmeghoo@gmail.com>
2024-04-10 12:50:44 +0100

    Add Appointment status Dropdown buttons for admin

--- MRMS/Enums/AppointmentStatus.cs
+++ MRMS/Enums/AppointmentStatus.cs
@@ -1,15 +1,38 @@
-﻿namespace MRMS.Enums
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MRMS.Enums
 {
     public enum AppointmentStatus
     {
+        [Display(Name =  "Arrival Pending")]
         ArrivalPending,
+
+        [Display(Name = "Arrived")]
         Arrived,
+
+        [Display(Name = "Appointment Missed")]
+        MissedAppointment,
+
+        [Display(Name = "Left Prematurely")]
         LeftPrematurely,
+
+        [Display(Name = "Nurse Consulting Patient")]
         NurseSentInPatient,
+
+        [Display(Name = "Nurse Consultation Completed")]
         SeenNurse,
+
+        [Display(Name = "Doctor Consulting Patient")]
         DoctorSentInPatient,
+
+        [Display(Name = "Appointment Completed")]
         AppointmentComplete,
+
+        [Display(Name = "Immediate Tests Underway")]
         ImmediateTestsOrdered,
+
+        [Display(Name = "Tests Completed")]
         TestsComplete
     }
 }
