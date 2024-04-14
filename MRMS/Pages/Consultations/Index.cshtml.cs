02e6911b40283d1a19f25e53543511aa33b209b7
Andreas Meghoo <andyjmeghoo@gmail.com>
2024-04-14 12:45:00 +0100

    Code change to limit consultations displayed
    When the logged in user is a patient, they can only view their own previous consultations

--- MRMS/Pages/Consultations/Index.cshtml.cs
+++ MRMS/Pages/Consultations/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -7,8 +8,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using MRMS.Data;
 using MRMS.Models;
+using MRMS.Pages.Appointments;
 
 namespace MRMS.Pages.Consultations
 {
@@ -30,11 +33,40 @@ namespace MRMS.Pages.Consultations
         public async Task OnGetAsync()
         {
             UserId = _userManager.GetUserId(User);
+            
 
-            if (_context.Consultation != null)
+            if (_context.Consultation != null && _context.Appointment != null)
             {
-                Consultation = await _context.Consultation.ToListAsync();
+                IList<Consultation> AllConsultations = await _context.Consultation.ToListAsync();
+                IList<Appointment> AllAppointments = await _context.Appointment.ToListAsync();
+                IList<int> CurrentUserAppointments = new List<int>();
+                
+                foreach (Appointment appointment in AllAppointments)
+                {
+                    if(appointment.PatientId == UserId)
+                    {
+                        CurrentUserAppointments.Add(appointment.AppointmentId);
+                    }
+                }
+
+                if (!User.IsInRole("patient"))
+                {
+                    Consultation = AllConsultations;
+                }
+                else
+                {
+                    Consultation = new List<Consultation>();
+                    foreach (Consultation consultation in AllConsultations)
+                    {
+                        if(CurrentUserAppointments.Contains(consultation.AppointmentId))
+                        {
+                            Consultation.Add(consultation);
+                        }
+                    }
+                }
+
             }
+
         }
     }
 }
