cf48d6455ff1c5db84a1d042bae439e9793f890b
Andreas Meghoo <andyjmeghoo@gmail.com>
2024-04-18 23:33:26 +0100

    Only display time slots that are available when requesting an appointment.

--- MRMS/Pages/Appointments/Create.cshtml.cs
+++ MRMS/Pages/Appointments/Create.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Scripting;
 using MRMS.Data;
 using MRMS.Models;
@@ -53,7 +54,7 @@ namespace MRMS.Pages.Appointments
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync(string time, string date)
         {
-          if (!ModelState.IsValid || _context.Appointment == null || Appointment == null)
+            if (!ModelState.IsValid || _context.Appointment == null || Appointment == null)
             {
                 return Page();
             }
@@ -73,19 +74,16 @@ namespace MRMS.Pages.Appointments
             var doctors = _userManager.GetUsersInRoleAsync("doctor").Result.ToList();
             if (User.IsInRole("patient"))
             {
-                Appointment = new Appointment();
-                Appointment.PatientId = _userManager.GetUserId(User);
-                Appointment.PreferredDoctorId = doctorId;
                 Date = DateOnly.TryParse(date, out DateOnly parsedDate) ? parsedDate : default;
                 ModelState.ClearValidationState("Appointment.Reason");
 
-                AvailableTimeSlots = GenerateTimeSlots();
+                AvailableTimeSlots = GenerateTimeSlots(doctorId, Date);
             }
             Doctors = doctors;
             return Page();
         }
 
-        private List<(TimeOnly, TimeOnly)> GenerateTimeSlots()
+        private List<(TimeOnly, TimeOnly)> GenerateTimeSlots(string doctorId, DateOnly date)
         {
             List<(TimeOnly, TimeOnly)> timeSlots = new List<(TimeOnly, TimeOnly)>();
 
@@ -94,22 +92,46 @@ namespace MRMS.Pages.Appointments
             TimeOnly afternoonStart = new TimeOnly(14, 0);
             TimeOnly afternoonEnd = new TimeOnly(18, 0);
 
+            var doctorAppointments = _context.Appointment
+                                        .Where(a => a.PreferredDoctorId == doctorId &&
+                                        a.StartTime.Date == date.ToDateTime(new TimeOnly(0, 0)).Date)
+                                        .ToList();
+
             TimeOnly i = morningStart;
             while (i < morningEnd)
             {
-                timeSlots.Add((i, i.AddMinutes(15)));
+                if (!IsTimeSlotBooked(doctorAppointments, i, i.AddMinutes(15),date))
+                {
+                    timeSlots.Add((i, i.AddMinutes(15)));
+                }
                 i = i.AddMinutes(15);
             }
 
             i = afternoonStart;
             while (i < afternoonEnd)
             {
-                timeSlots.Add((i, i.AddMinutes(15)));
+                if (!IsTimeSlotBooked(doctorAppointments, i, i.AddMinutes(15), date))
+                {
+                    timeSlots.Add((i, i.AddMinutes(15)));
+                }
                 i = i.AddMinutes(15);
             }
 
             return timeSlots;
         }
 
+        private bool IsTimeSlotBooked(List<Appointment> doctorAppointments, TimeOnly startTime, TimeOnly endTime, DateOnly date)
+        {
+            foreach (var appointment in doctorAppointments)
+            {
+                DateTime slotStartDateTime = date.ToDateTime(startTime);
+                DateTime slotEndDateTime = date.ToDateTime(endTime);
+                if (appointment.StartTime < slotEndDateTime && appointment.EndTime > slotStartDateTime)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
