1555cc47bb7baa877e92a815fc425dcdf91ae5b6
Andreas Meghoo <andyjmeghoo@gmail.com>
2024-05-03 06:13:44 +0100

    All create pages - usability improvements (staff view)

--- MRMS/Pages/BloodTests/Create.cshtml.cs
+++ MRMS/Pages/BloodTests/Create.cshtml.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using MRMS.Data;
 using MRMS.Models;
+using NuGet.Packaging;
 
 namespace MRMS.Pages.BloodTests
 {
@@ -15,20 +17,43 @@ namespace MRMS.Pages.BloodTests
     public class CreateModel : PageModel
     {
         private readonly MRMS.Data.MRMSContext _context;
+        private readonly UserManager<User> _userManager;
 
-        public CreateModel(MRMS.Data.MRMSContext context)
+        public CreateModel(MRMS.Data.MRMSContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult OnGet()
         {
+            Patients = _userManager.GetUsersInRoleAsync("patient").Result.ToList();
+            Doctors = _userManager.GetUsersInRoleAsync("doctor").Result.ToList();
+            var nurses = _userManager.GetUsersInRoleAsync("nurse").Result.ToList();
+            Staff.AddRange(nurses);
+            Staff.AddRange(Doctors);
+
+            if (_context.Consultation != null && _context.Appointment != null)
+            {
+                Consultations = _context.Consultation.ToList();
+                Appointments = _context.Appointment.ToList();
+            }
             return Page();
         }
 
         [BindProperty]
         public BloodTest BloodTest { get; set; } = default!;
-        
+
+        public IList<Consultation> Consultations { get; set; }
+
+        public IList<Appointment> Appointments { get; set; }
+
+        public IList<User> Patients { get; set; }
+
+        public IList<User> Doctors { get; set; }
+
+        public List<User> Staff { get; set; } = new List<User>();
+
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
--- MRMS/Pages/Prescriptions/Create.cshtml.cs
+++ MRMS/Pages/Prescriptions/Create.cshtml.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using MRMS.Data;
+using MRMS.Enums;
 using MRMS.Models;
 
 namespace MRMS.Pages.Prescriptions
@@ -15,20 +17,37 @@ namespace MRMS.Pages.Prescriptions
     public class CreateModel : PageModel
     {
         private readonly MRMS.Data.MRMSContext _context;
+        private readonly UserManager<User> _userManager;
 
-        public CreateModel(MRMS.Data.MRMSContext context)
+        public CreateModel(MRMS.Data.MRMSContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult OnGet()
         {
+            Patients = _userManager.GetUsersInRoleAsync("patient").Result.ToList();
+            Doctors = _userManager.GetUsersInRoleAsync("doctor").Result.ToList();
+            if (_context.Consultation != null && _context.Appointment != null)
+            {
+                Consultations = _context.Consultation.ToList();
+                Appointments = _context.Appointment.ToList();
+            }
             return Page();
         }
 
         [BindProperty]
         public Prescription Prescription { get; set; } = default!;
-        
+
+        public IList<Consultation> Consultations { get; set; }
+
+        public IList<Appointment> Appointments { get; set; }
+
+        public IList<User> Patients { get; set; }
+
+        public IList<User> Doctors { get; set; }
+ 
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
